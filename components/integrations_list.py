bd0ad49a4fb3bc5d7b575312192bf03f5417883c
AD <aradubrovskiy@beeline.ru>
2021-09-28 11:57:21 +0300

    cleanup

--- components/integrations_list.py
+++ components/integrations_list.py
@@ -8,17 +8,12 @@ from pydantic import parse_obj_as
 
 def render_integrations(context, slot, payload):
 
-
     results = context.rpc_manager.call.integrations_get_project_integrations(payload['id'])
 
-    print('existing_integrations', results)
+    # print('existing_integrations', results)
 
     payload['existing_integrations'] = results
     payload['integrations_sections'] = {*context.rpc_manager.call.integrations_sections(), *results.keys()}
-
-    # payload['scanners'] = "security_scanners"
-    # payload["right_scanners"] = "right_col_scanners"
-    # payload["left_scanners"] = "left_col_scanners"
 
     return render_template(
         'integrations_list.html',
