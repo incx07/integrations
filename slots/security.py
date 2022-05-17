d001c8130e0997e191027d0a278988f05c5e9cbf
aspect13 <ad13box@gmail.com>
2022-05-17 09:54:18 +0200

    fixing integrations remastering zap

--- slots/security.py
+++ slots/security.py
@@ -7,10 +7,24 @@ class Slot:
 
     @web.slot('integrations_security_app_content')
     def security_app_content(self, context, slot, payload):
-        # integrations_section_list = context.rpc_manager.call.integrations_section_list()
         with context.app.app_context():
             return self.descriptor.render_template(
-                'security/app.html',
-                # integrations_section_list=integrations_section_list
+                'security/app/content.html',
+                integrations_section_list=self.section_list()
+            )
+
+    @web.slot('integrations_security_app_scripts')
+    def security_app_scripts(self, context, slot, payload):
+        with context.app.app_context():
+            return self.descriptor.render_template(
+                'security/app/scripts.html',
+                integrations_section_list=self.section_list()
+            )
+
+    @web.slot('integrations_security_app_styles')
+    def security_app_styles(self, context, slot, payload):
+        with context.app.app_context():
+            return self.descriptor.render_template(
+                'security/app/styles.html',
                 integrations_section_list=self.section_list()
             )
--- static/js/security_app_create.js
+++ static/js/security_app_create.js
@@ -1,14 +1,19 @@
 $(() => {
     new SectionDataProvider('integrations', {
-        get: () => (
-            $('.integration_section').toArray().reduce((accum, item) => {
+        get: () => {
+            let all_data = $('.integration_section').toArray().reduce((accum, item) => {
                 const sectionElement = $(item)
                 const sectionName = sectionElement.find('.integration_section_name').text().toLowerCase().replace(' ', '_')
-
-                const sectionData = sectionElement.find('.security_integration_item').toArray().reduce((acc, i) => {
+                let sectionData = sectionElement.find('.security_integration_item').toArray().reduce((acc, i) => {
                     const integrationName = i.dataset.name
                     const dataCallbackName = `${sectionName}_${integrationName}`
-                    const integrationData = window[dataCallbackName]?.get_data()
+                    let integrationData
+                    if (window[dataCallbackName]) {
+                        integrationData = window[dataCallbackName].get_data()
+                    } else {
+                        // integrationData = vueVm.registered_components[integrationName]?.get_data()
+                    }
+
                     if (integrationData) {
                         acc[integrationName] = integrationData
                     }
@@ -20,7 +25,16 @@ $(() => {
                 }
                 return accum;
             }, {})
-        ),
+            Object.entries(vueVm.registered_components).reduce((acc, [name, i]) => {
+                const tmp_data = i.get_data && i.get_data()
+                if (tmp_data && Object.entries(tmp_data).length) {
+                    acc[i.section] = acc[i.section] || {}
+                    acc[i.section][name] = tmp_data
+                }
+                return acc
+            }, all_data)
+            return all_data
+        },
         set: values => {
             if (values) {
                 console.log('SET integrations', values)
@@ -35,7 +49,11 @@ $(() => {
                 Object.keys(values).forEach(section => {
                     Object.keys(values[section]).forEach(integrationItem => {
                         const dataCallbackName = `${section}_${integrationItem}`
-                        window[dataCallbackName]?.set_data(values[section][integrationItem])
+                        if (window[dataCallbackName]) {
+                            window[dataCallbackName].set_data(values[section][integrationItem])
+                        } else {
+                            vueVm.registered_components[integrationItem]?.set_data(values[section][integrationItem])
+                        }
                     })
                 })
             }
@@ -48,15 +66,31 @@ $(() => {
                 sectionElement.find('.security_integration_item').toArray().forEach(i => {
                     const integrationName = $(i).attr('data-name')
                     const dataCallbackName = `${sectionName}_${integrationName}`
-                    window[dataCallbackName]?.clear_data()
+
+                    if (window[dataCallbackName]) {
+                        window[dataCallbackName].clear_data()
+                    } else {
+                        // vueVm.registered_components[integrationName]?.clear_data()
+                    }
                 })
+                Object.values(vueVm.registered_components).forEach(i => {
+                    if (i.section) {
+                        i.clear_data()
+                    }
+                })
+
             })
         ),
         setError: data => {
             console.log(data)
             const [dataCallbackName, ...rest] = data.loc
             data.loc = rest
-            window[dataCallbackName]?.set_error(data)
+            if (window[dataCallbackName]) {
+                window[dataCallbackName].set_error(data)
+            } else {
+                // vueVm.registered_components[integrationName]?.set_error(data)
+                console.warn('SET ERROR FAIL', dataCallbackName, data.loc)
+            }
         },
         clearErrors: () => {
             $('.integration_section').toArray().forEach(item => {
@@ -65,10 +99,106 @@ $(() => {
                 sectionElement.find('.security_integration_item').toArray().forEach(i => {
                     const integrationName = $(i).attr('data-name')
                     const dataCallbackName = `${sectionName}_${integrationName}`
-                    window[dataCallbackName]?.clear_errors && window[dataCallbackName]?.clear_errors()
+                    if (window[dataCallbackName]) {
+                        window[dataCallbackName].clear_errors && window[dataCallbackName].clear_errors()
+                    } else {
+                        vueVm.registered_components[integrationName]?.clear_errors && vueVm.registered_components[integrationName]?.clear_errors()
+                    }
                 })
             })
         },
         default: {}
     }).register()
 })
+
+const TestIntegrationItem = {
+    delimiters: ['[[', ']]'],
+    props: ['integration_name', 'display_name', 'project_integrations'],
+    data() {
+        return {
+            selected_integration: undefined,
+            is_selected: false,
+        }
+    },
+    mounted() {
+        this.selected_integration = this.default_integration?.id
+    },
+    computed: {
+        settings_id() {
+            return `settings_${this.integration_name}`
+        },
+        selector_id() {
+            return `selector_${this.integration_name}`
+        },
+        default_integration() {
+            return this.project_integrations.find(item => item.is_default)
+        }
+    },
+    methods: {
+        getIntegrationTitle(integration) {
+            return integration.is_default ? `${integration.description} - default` : integration.description
+        },
+        clear_data() {
+            console.log('CLEAR_DATA TestIntegrationItem')
+            this.is_selected = false
+            $(this.$el).collapse('hide')
+            this.selected_integration = this.default_integration?.id
+        },
+        set_data() {
+            this.is_selected = true
+            $(this.$el).collapse('show')
+        },
+
+    },
+    template: `
+<div class="col-6">
+    <div class="card card-row-1">
+        <div class="card-header">
+            <div class="d-flex">
+                <h9 class="flex-grow-1" style="line-height: 24px">[[ display_name ]]</h9>
+                <button aria-expanded="false" 
+                        type="button"
+                        class="btn btn-24 btn-action"
+                        data-toggle="collapse" 
+                        :data-target="'#' + settings_id" 
+                        v-if="!!this.$slots.settings"
+                        >
+                    <i class="fas fa-cog"></i>
+                </button>
+                <label class="custom-toggle">
+                    <input aria-expanded="false" type="checkbox"
+                           :data-target="'#' + selector_id" data-toggle="collapse"
+                           v-model="is_selected"
+                           />
+                    <span class="custom-toggle-slider rounded-circle"></span>
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="collapse col-12 mb-3 pl-0" :id="selector_id">
+                <select class="selectpicker" data-style="btn-secondary"
+                    v-model="selected_integration">
+                    <option
+                        v-for="integration in project_integrations"
+                        :value="integration.id"
+                        :title="getIntegrationTitle(integration)"
+                    >
+                        [[ getIntegrationTitle(integration) ]]
+                    </option>
+                </select>
+                <slot name="settings"></slot>
+                <slot 
+                    name="selector"
+                    @set_data="set_data" 
+                    @clear_data="clear_data" 
+                    :selected_integration="selected_integration"
+                    :is_selected="is_selected"
+                ></slot>
+            </div>
+        </div>
+    </div>
+</div>
+    `,
+}
+
+vueApp.component('TestIntegrationItem', TestIntegrationItem)
